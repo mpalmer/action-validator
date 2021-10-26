435f06a7b09011e383ffdb36b550a3b5af9e9cfb
Matt Palmer <mpalmer@hezmatt.org>
2021-10-26 21:38:40 +1100

    Add glob validation

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use std::fs::File;
 
 pub use crate::config::Config;
 use crate::schemas::{validate_as_action, validate_as_workflow};
+use glob::glob;
 
 pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     let fd = File::open(&config.src)?;
@@ -35,16 +36,55 @@ pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
                     .ok_or("Unable to convert PathBuf to string!")?
             );
         }
-        validate_as_workflow(&doc)
+        validate_as_workflow(&doc) && validate_paths(&doc)
     };
 
     if valid_doc {
         Ok(())
     } else {
-        Err("failed schema validation".into())
+        Err("validation failed".into())
     }
 }
 
 fn parse_src(fd: std::fs::File) -> serde_yaml::Result<serde_json::Value> {
     serde_yaml::from_reader(fd)
+}
+
+fn validate_paths(doc: &serde_json::Value) -> bool {
+    let mut success = true;
+
+    success = validate_globs(&doc["on"]["push"]["paths"], "on.push.paths") && success;
+    success = validate_globs(&doc["on"]["push"]["paths-ignore"], "on.push.paths-ignore") && success;
+    success =
+        validate_globs(&doc["on"]["pull_request"]["paths"], "on.pull_request.paths") && success;
+    success = validate_globs(
+        &doc["on"]["pull_request"]["paths-ignore"],
+        "on.pull_request.paths-ignore",
+    ) && success;
+
+    success
+}
+
+fn validate_globs(globs: &serde_json::Value, path: &str) -> bool {
+    if globs.is_null() {
+        true
+    } else {
+        let mut success = true;
+
+        for g in globs.as_array().unwrap() {
+            match glob(g.as_str().unwrap()) {
+                Ok(res) => {
+                    if res.count() == 0 {
+                        eprintln!("Glob {} in {} does not match any files", g, path);
+                        success = false;
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Glob {} in {} is invalid: {}", g, path, e);
+                }
+            };
+        }
+
+        success
+    }
 }
