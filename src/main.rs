0fd5f355663a21a3a8ca2f71f48d488102c64cc7
Matthieu Bizien <matthieu@yokai.ai>
2023-02-23 18:10:12 +0100

    Allow multiple arguments in the CLI

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,27 @@ use std::process;
 fn main() {
     let config = Config::parse();
 
-    if let Err(e) = action_validator::run(&config) {
-        println!(
-            "Fatal error validating {}: {}",
-            config.src.to_str().unwrap(),
-            e
-        );
+    let mut has_errors = false;
+
+    // Run the validator on each file and collect any errors
+    config
+        .src
+        .iter()
+        .map(|src| (src, action_validator::run_on_file(src, config.verbose)))
+        .filter_map(|(src, result)| match result {
+            Ok(_) => None,
+            Err(error) => Some((src, error)),
+        })
+        .for_each(|(src, error)| {
+            println!(
+                "Fatal error validating {}: {}",
+                src.to_str().unwrap(),
+                error
+            );
+            has_errors = true;
+        });
+
+    if has_errors {
         process::exit(1);
     }
 }
