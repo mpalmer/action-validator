1d2be7965335d31c1c70a37833ab3603aa892d5a
Antti Harju <antti@harju.io>
2026-04-10 18:16:56 +0300

    Fix npm test

--- src/js/system.js
+++ src/js/system.js
@@ -1,9 +1,19 @@
 const console = require('node:console');
 const fs = require('node:fs');
 const process = require('node:process');
-const fg = require('fast-glob');
+const childProcess = require('node:child_process');
+
+const git = {
+	lsFiles() {
+		const output = childProcess.execFileSync('git', ['ls-files', '-z'], {
+			encoding: 'utf8',
+		});
+
+		return output.split('\0').filter((path) => path.length > 0);
+	},
+};
 
 module.exports.console = console;
 module.exports.fs = fs;
 module.exports.process = process;
-module.exports.fg = fg;
+module.exports.git = git;
--- src/system/git.rs
+++ src/system/git.rs
@@ -1,20 +1,44 @@
-use std::process::Command;
+#[cfg(feature = "js")]
+mod platform {
+    use wasm_bindgen::prelude::*;
 
-pub fn ls_files() -> Result<Vec<String>, std::io::Error> {
-    let output = Command::new("git").args(["ls-files", "-z"]).output()?;
-
-    if !output.status.success() {
-        return Err(std::io::Error::other(format!(
-            "git ls-files failed: {}",
-            String::from_utf8_lossy(&output.stderr)
-        )));
+    #[wasm_bindgen(module = "/src/js/system.js")]
+    extern "C" {
+        #[wasm_bindgen(catch, js_namespace = git, js_name = lsFiles)]
+        fn ls_files_js() -> Result<js_sys::Array, js_sys::Error>;
     }
 
-    let files = String::from_utf8_lossy(&output.stdout)
-        .split('\0')
-        .filter(|s| !s.is_empty())
-        .map(|s| s.to_string())
-        .collect();
+    pub(super) fn ls_files() -> Result<Vec<String>, std::io::Error> {
+        let files = ls_files_js().map_err(|e| std::io::Error::other(format!("{e:?}")))?;
 
-    Ok(files)
+        Ok(files.iter().filter_map(|value| value.as_string()).collect())
+    }
+}
+
+#[cfg(not(feature = "js"))]
+mod platform {
+    pub(super) fn ls_files() -> Result<Vec<String>, std::io::Error> {
+        use std::process::Command;
+
+        let output = Command::new("git").args(["ls-files", "-z"]).output()?;
+
+        if !output.status.success() {
+            return Err(std::io::Error::other(format!(
+                "git ls-files failed: {}",
+                String::from_utf8_lossy(&output.stderr)
+            )));
+        }
+
+        let files = String::from_utf8_lossy(&output.stdout)
+            .split('\0')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.to_string())
+            .collect();
+
+        Ok(files)
+    }
+}
+
+pub fn ls_files() -> Result<Vec<String>, std::io::Error> {
+    platform::ls_files()
 }
